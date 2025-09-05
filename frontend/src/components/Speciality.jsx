66ffe80b6929ae13909955032d9b9d4dfa83e9fc
yasindu2000 <yasindudahanayake@gmail.com>
2025-09-05 19:33:24 +0530

    complete speciality coponent🚀

--- frontend/src/components/Speciality.jsx
+++ frontend/src/components/Speciality.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from "react";
+import { specialityData } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 function Speciality() {
   return (
-    <div id='speciality'>
-        <h1 className="">Find by Speciality</h1>
+    <div className="flex flex-col items-center gap-4 py-16 text-gray-800" id="speciality">
+      <h1 className="text-3xl font-medium">Find by Speciality</h1>
+      <p className="sm:w-1/3 text-center text-sm">Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
+      <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-scroll ">
+        {
+            specialityData.map((item, index)=>{
+              return  <Link onClick={()=>scrollTo(0, 0)} className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500" key={index} to={`/doctors/${item.speciality}`} >
+                    <img src={item.image} alt="" className="w-16 sm:w-24 mb-2 " />
+                    <p className="text-center">{item.speciality}</p>
+                
+                </Link>
 
-
+            })
+        }
+      </div>
     </div>
-  )
+  );
 }
 
-export default Speciality
+export default Speciality;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import Speciality from '../components/Speciality'
 
 function Home() {
   return (
     <div>
       <Header/>
+      <Speciality/>
     </div>
   )
 }
