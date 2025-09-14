554f05678944a6cb789f6613d90eceb6b83c8453
yasindu2000 <yasindudahanayake@gmail.com>
2025-09-14 09:41:24 +0530

    complete banner component🚀

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Speciality from '../components/Speciality'
 import TopDoctors from '../components/TopDoctors'
+import Banner from '../components/Banner'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Header/>
       <Speciality/>
       <TopDoctors/>
+      <Banner/>
     </div>
   )
 }
